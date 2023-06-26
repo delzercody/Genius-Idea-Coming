b3bc354cb567e29ce7d0ed24799c62b801b618b8
Eric <macario41@gmail.com>
2023-06-26 17:05:48 -0400

    sidebar stuff

--- Frontend/src/components/Category.js
+++ Frontend/src/components/Category.js
@@ -1,17 +1,29 @@
-import Sidebar from "./Sidebar"
+import Sidebar from "./Sidebar";
+import NavBar from "./NavBar";
+import '../stylesheets/Category.css';
+
 function Category() {
   return (
+    <div>
+      <NavBar />
+      <div className="headerimage d-flex justify-content-center">
+        insert giant image here according to the category
+      </div>
       <div className="container">
-        <div className="row">
-          <div className="col-md-3">
+        <div className="row justify-content-left align-items-stretch">
+          <div className="col-md-2 custom-height sidebar-wrapper">
             <Sidebar />
           </div>
-          <div className="col-md-9">
-            Hi
+          <div className="col-md-5 d-flex justify-content-center align-items-center">
+            <button type="button" className="btn btn-secondary">Create an idea</button>
+          </div>
+          <div className="col-md-5 d-flex justify-content-center align-items-center">
+          <button type="button" className="btn btn-secondary">Random button</button>
           </div>
         </div>
       </div>
-  )
+    </div>
+  );
 }
 
-export default Category
+export default Category;
