8685c413b5571236d5aa3021daaa2bddb5afddc6
Eric <macario41@gmail.com>
2023-06-26 18:33:53 -0400

    Category

--- Frontend/src/components/Category.js
+++ Frontend/src/components/Category.js
@@ -1,13 +1,26 @@
+import { useState } from 'react';
 import Sidebar from "./Sidebar";
 import NavBar from "./NavBar";
+import {Link} from 'react-router-dom'
 import '../stylesheets/Category.css';
 
 function Category() {
+  const [cards, setCards] = useState([]);
+
+  const generateIdea = () => {
+    const newCard = {
+      id: cards.length + 1,
+      title: "New Card Title",
+      content: "New Card Content"
+    };
+    setCards(prevCards => [...prevCards, newCard]);
+  };
+
   return (
     <div>
       <NavBar />
       <div className="headerimage d-flex justify-content-center">
-        insert giant image here according to the category
+        This is a giant image
       </div>
       <div className="container">
         <div className="row justify-content-left align-items-stretch">
@@ -15,10 +28,34 @@ function Category() {
             <Sidebar />
           </div>
           <div className="col-md-5 d-flex justify-content-center align-items-center">
-            <button type="button" className="btn btn-secondary">Create an idea</button>
+            <button type="button" className="btn btn-secondary" onClick={generateIdea}>
+              Generate an idea
+            </button>
           </div>
           <div className="col-md-5 d-flex justify-content-center align-items-center">
-          <button type="button" className="btn btn-secondary">Random button</button>
+          <Link to="/Form" className="btn btn-secondary">Create an Idea</Link>
+          </div>
+        </div>
+        <div className="row mt-4">
+          <div className="col-md-10 offset-md-2">
+            <div className="card-container" style={{ marginTop: '300px' }}>
+              {cards.map(card => (
+                <div className="card mb-3" key={card.id}>
+                  <div className="card-body">
+                    <div className="card-title text-center">{card.title}</div>
+                    <div className="card-text text-center">{card.content}</div>
+                    <div className="d-flex justify-content-between mt-2">
+                      <div>
+                        <a href="#" className="btn btn-primary">Add to Collection</a>
+                      </div>
+                      <div>
+                        <a href="#" className="btn btn-primary">Comment</a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
           </div>
         </div>
       </div>
