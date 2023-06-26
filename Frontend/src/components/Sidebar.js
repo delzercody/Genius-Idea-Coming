3448c4ab1a899831c3e1a6d799149d8e538ec5aa
Eric <macario41@gmail.com>
2023-06-26 15:24:51 -0400

    bootstrap

--- Frontend/src/components/Sidebar.js
+++ Frontend/src/components/Sidebar.js
@@ -1,7 +1,28 @@
+import {Link} from 'react-router-dom'
+
 function Sidebar() {
   return (
-    <div>Sidebar</div>
-  )
+      <div className="list-group">
+        <h5 className="list-group-item">
+          <Link className='sidebar-link' to="/Form">Form</Link>
+        </h5>
+        <h5 className="list-group-item">
+          <Link className='sidebar-link' to="/Category/cooking">Cooking</Link>
+        </h5>
+        <h5 className="list-group-item">
+          <Link className='sidebar-link' to="/Category/technology">Technology</Link>
+        </h5>
+        <h5 className="list-group-item">
+          <Link className='sidebar-link' to="/Category/home">Home Improvement</Link>
+        </h5>
+        <h5 className="list-group-item">
+          <Link className='sidebar-link' to="/Category/art">Art</Link>
+        </h5>
+        <h5 className="list-group-item">
+          <Link className='sidebar-link' to="/Category/business">Business</Link>
+        </h5>
+      </div>
+  );
 }
 
 export default Sidebar
