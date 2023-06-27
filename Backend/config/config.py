edf003a874ba6336b3648672252e58be99a2543b
Melissa Elizondo <melissaelizn@gmail.com>
2023-06-26 22:49:26 -0400

    added validations and login/signup

--- Backend/config/config.py
+++ Backend/config/config.py
@@ -1,8 +1,15 @@
-SQLALCHEMY_DATABASE_URI = 'sqlite:///generate_ideas.db'
-SQLALCHEMY_TRACK_MODIFICATIONS = False
 from flask import Flask
 from flask_cors import CORS
 from flask_migrate import Migrate
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///generate_ideas.db'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+from flask_bcrypt import Bcrypt
+
+app = Flask(__name__)
+
+bcrypt = Bcrypt(app)
